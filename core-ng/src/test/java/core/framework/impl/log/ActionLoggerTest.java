03810aee04be270682ec97c42e91bcd3037a4b0c
neo <neowu.us@gmail.com>
2015-09-22 16:17:39 -0400

    refactory log impl

---
+++ core-ng/src/test/java/core/framework/impl/log/ActionLoggerTest.java
@@ -0,0 +1,24 @@
+package core.framework.impl.log;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author neo
+ */
+public class ActionLoggerTest {
+    ActionLogger actionLogger;
+
+    @Before
+    public void createActionLogger() {
+        actionLogger = ActionLogger.console();
+    }
+
+    @Test
+    public void filterLineSeparator() {
+        String message = "line1\nline2";
+        String filteredMessage = actionLogger.filterLineSeparator(message);
+        Assert.assertEquals("line1 line2", filteredMessage);
+    }
+}
