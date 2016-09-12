27977e86aaab0fd47513c57b10fcdb1786f192a3
neo <neowu.us@gmail.com>
2016-09-12 14:33:36 -0400

    update node config

--- demo-site/gulpfile.js
+++ demo-site/gulpfile.js
@@ -1,79 +1,73 @@
-var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var md5 = require("gulp-md5-plus");
-var merge = require('merge2');
+const gulp = require("gulp");
+const sourcemaps = require("gulp-sourcemaps");
+const md5 = require("gulp-md5-plus");
+const merge = require("merge2");
 
-gulp.task('clean', function() {
-    var del = require('del');
-    return del(['src/main/dist/web/template', 'src/main/dist/web/static/css', 'src/main/dist/web/static/js'])
+gulp.task("clean", function() {
+    const del = require("del");
+    return del(["src/main/dist/web/template", "src/main/dist/web/static/css", "src/main/dist/web/static/js"])
 });
 
-gulp.task('html', function() {
-    return gulp.src('src/main/web/template/**/*.html')
-        .pipe(gulp.dest('src/main/dist/web/template'))
+gulp.task("html", function() {
+    return gulp.src("src/main/web/template/**/*.html")
+        .pipe(gulp.dest("src/main/dist/web/template"))
 })
 
 gulp.task("css", ["html"], function() {
-    var postcss = require('gulp-postcss');
-    var stylelint = require('stylelint');
-    var reporter = require('postcss-reporter');
-    var autoprefixer = require('autoprefixer');
-    var cssnano = require('cssnano');
+    const stylelint = require("gulp-stylelint");
+    const cssnano = require("gulp-cssnano");
 
-    var processors = [
-        require('precss'),
-        stylelint({
-            configFile: 'stylelint.json'
-        }),
-        reporter({
-            clearMessages: true,
-            throwError: true
-        }),
-        autoprefixer(),
-        cssnano()
-    ];
-
-    var appCSS = gulp.src(['src/main/web/static/css/**/*.css', '!src/main/web/static/css/vendor{,/**/*.css}'])
+    var appCSS = gulp.src(["src/main/web/static/css/**/*.css", "!src/main/web/static/css/lib{,/**/*.css}"])
         .pipe(sourcemaps.init())
-        .pipe(postcss(processors))
-        .pipe(md5(10, 'src/main/dist/web/template/**/*.html'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('src/main/dist/web/static/css'));
+        .pipe(stylelint({
+            configFile: "stylelint.json",
+            reporters: [{
+                formatter: "string",
+                console: true
+            }]
+        }))
+        .pipe(cssnano())
+        .pipe(md5(10, "src/main/dist/web/template/**/*.html"))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("src/main/dist/web/static/css"));
 
-    var vendorCSS = gulp.src(['src/main/web/static/css/vendor{,/**/*.css}'])
-        .pipe(md5(10, 'src/main/dist/web/template/**/*.html'))
-        .pipe(gulp.dest('src/main/dist/web/static/css/vendor'));
+    var libCSS = gulp.src(["src/main/web/static/css/lib/*.css"])
+        .pipe(md5(10, "src/main/dist/web/template/**/*.html"))
+        .pipe(gulp.dest("src/main/dist/web/static/css/lib"));
 
-    return merge(appCSS, vendorCSS);
+    var fonts = gulp.src(["src/main/web/static/css/fonts/*"])
+        .pipe(gulp.dest("src/main/dist/web/static/css/fonts"));
+
+    return merge(appCSS, libCSS, fonts);
 });
 
-gulp.task('js', ["html"], function(cb) {
-    var uglify = require('gulp-uglify');
-    var eslint = require('gulp-eslint');
+gulp.task("js", ["html"], function(cb) {
+    const uglify = require("gulp-uglify");
+    const eslint = require("gulp-eslint");
 
-    var appJS = gulp.src(['src/main/web/static/js/**/*.js', '!src/main/web/static/js/vendor{,/**/*.js}'])
+    var appJS = gulp.src(["src/main/web/static/js/**/*.js", "!src/main/web/static/js/lib{,/**/*.js}"])
         .pipe(eslint({
-            configFile: 'eslint.json'
+            configFile: "eslint.json"
         }))
         .pipe(eslint.format())
         .pipe(eslint.failAfterError())
         .pipe(sourcemaps.init())
         .pipe(uglify())
-        .pipe(md5(10, 'src/main/dist/web/template/**/*.html'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('src/main/dist/web/static/js'));
+        .pipe(md5(10, "src/main/dist/web/template/**/*.html"))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("src/main/dist/web/static/js"));
 
-    var vendorJS = gulp.src(['src/main/web/static/js/vendor/**/*.js'])
-        .pipe(md5(10, 'src/main/dist/web/template/**/*.html'))
-        .pipe(gulp.dest('src/main/dist/web/static/js/vendor'));
+    var libJS = gulp.src(["src/main/web/static/js/lib/**/*.js"])
+        .pipe(md5(10, "src/main/dist/web/template/**/*.html"))
+        .pipe(gulp.dest("src/main/dist/web/static/js/lib"));
 
-    return merge(appJS, vendorJS);
+    return merge(appJS, libJS);
 });
 
-gulp.task('default', ['clean'], function() {
-    gulp.start('html', 'css', 'js');
+gulp.task("default", ["clean"], function() {
+    gulp.start("html", "css", "js");
 })
 
-gulp.task('watch', function() {
-    gulp.watch(['src/main/web/static/css/**/*.css', 'src/main/web/static/js/**/*.js', 'src/main/web/template/**/*.html'], ['js', 'css']);
+gulp.task("watch", function() {
+    gulp.watch(["src/main/web/static/css/**/*.css", "src/main/web/static/js/**/*.js", "src/main/web/template/**/*.html"], ["js", "css"]);
 });
