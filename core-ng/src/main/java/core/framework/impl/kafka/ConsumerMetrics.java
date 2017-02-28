f2615dbd3406a0176227466a589b46eb69d3eae9
neo <neowu.us@gmail.com>
2017-02-28 11:37:19 -0500

    * kafka: monitor max commit latency

--- core-ng/src/main/java/core/framework/impl/kafka/ConsumerMetrics.java
+++ core-ng/src/main/java/core/framework/impl/kafka/ConsumerMetrics.java
@@ -16,8 +16,9 @@ public class ConsumerMetrics implements Metrics {
     private final Map<String, Metric> recordsConsumedRate = Maps.newConcurrentHashMap();
     private final Map<String, Metric> bytesConsumedRate = Maps.newConcurrentHashMap();
     private final Map<String, Metric> fetchRate = Maps.newConcurrentHashMap();
+    private final Map<String, Metric> commitLatencyMax = Maps.newConcurrentHashMap();
 
-    public ConsumerMetrics(String name) {
+    ConsumerMetrics(String name) {
         this.name = name;
     }
 
@@ -28,6 +29,7 @@ public class ConsumerMetrics implements Metrics {
             stats.put(statName("records_consumed_rate"), sum(recordsConsumedRate));
             stats.put(statName("bytes_consumed_rate"), sum(bytesConsumedRate));
             stats.put(statName("fetch_rate"), sum(fetchRate));
+            stats.put(statName("commit_max_latency"), max(commitLatencyMax));
         }
     }
 
@@ -39,6 +41,8 @@ public class ConsumerMetrics implements Metrics {
                 else if ("records-consumed-rate".equals(name.name())) recordsConsumedRate.put(clientId, entry.getValue());
                 else if ("bytes-consumed-rate".equals(name.name())) bytesConsumedRate.put(clientId, entry.getValue());
                 else if ("fetch-rate".equals(name.name())) fetchRate.put(clientId, entry.getValue());
+            } else if ("consumer-coordinator-metrics".equals(name.group())) {
+                if ("commit-latency-max".equals(name.name())) commitLatencyMax.put(clientId, entry.getValue());
             }
         }
     }
@@ -48,10 +52,15 @@ public class ConsumerMetrics implements Metrics {
         recordsConsumedRate.remove(clientId);
         bytesConsumedRate.remove(clientId);
         fetchRate.remove(clientId);
+        commitLatencyMax.remove(clientId);
     }
 
     private double sum(Map<String, Metric> metrics) {
         return metrics.values().stream().mapToDouble(Metric::value).filter(Double::isFinite).sum();
+    }
+
+    private double max(Map<String, Metric> metrics) {
+        return metrics.values().stream().mapToDouble(Metric::value).filter(Double::isFinite).max().orElse(0);
     }
 
     String statName(String statName) {
