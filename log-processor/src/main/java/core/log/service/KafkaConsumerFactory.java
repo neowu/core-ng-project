4d4a9f8c0bcf90f1f867dc9854be2abaee6a0077
neo <neowu.us@gmail.com>
2017-03-23 12:06:14 -0400

    update log processor to make it docker/kube friendly

--- log-processor/src/main/java/core/log/service/KafkaConsumerFactory.java
+++ log-processor/src/main/java/core/log/service/KafkaConsumerFactory.java
@@ -24,6 +24,7 @@ public class KafkaConsumerFactory {
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, uri);
         config.put(ConsumerConfig.GROUP_ID_CONFIG, "log-processor");
         config.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        config.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 2000);
         config.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, 3 * 1024 * 1024); // get 3M message at max
         config.put(ConsumerConfig.FETCH_MIN_BYTES_CONFIG, 1024 * 1024); // try to get at least 1M message
         config.put(ConsumerConfig.FETCH_MAX_WAIT_MS_CONFIG, 500);       // pause 500ms if not enable data to process
