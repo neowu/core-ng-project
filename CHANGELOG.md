4fd1b7cd6a525d897e3d9217d579554f0a51ee12
neo <neowu.us@gmail.com>
2017-10-23 12:54:45 -0400

    * web: @QueryParam bean validation to disallow @Property

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 ## Change log
 ### 5.1.1 (10/17/2017 - )
 * pool: refactor and simplify resource pool
+* web: @QueryParam bean validation to disallow @Property
 
 ### 5.1.0 (10/13/2017 - 10/16/2017)
 * test: updated to junit 5, for old tests before upgrading API, add following dependency 'junit:junit:4.12' 'org.junit.vintage:junit-vintage-engine:4.12.1'
--- core-ng/src/test/java/core/framework/impl/web/bean/QueryParamBeanTypeValidatorTest.java
+++ core-ng/src/test/java/core/framework/impl/web/bean/QueryParamBeanTypeValidatorTest.java
@@ -1,9 +1,6 @@
 package core.framework.impl.web.bean;
 
-import core.framework.api.web.service.QueryParam;
 import org.junit.jupiter.api.Test;
-
-import java.time.LocalDateTime;
 
 /**
  * @author neo
@@ -12,16 +9,5 @@ class QueryParamBeanTypeValidatorTest {
     @Test
     void beanType() {
         new QueryParamBeanTypeValidator(TestQueryParamBean.class).validate();
-    }
-
-    public static class TestQueryParamBean {
-        @QueryParam(name = "int_field")
-        public Integer intField;
-
-        @QueryParam(name = "string_field")
-        public String stringField;
-
-        @QueryParam(name = "date_time_field")
-        public LocalDateTime dateTimeField;
     }
 }
