1b5affda3935d6e33ca59b664cdf52d276dd1491
neo <neowu.us@gmail.com>
2018-07-12 19:00:21 -0400

    fix pmd issues

--- core-ng/src/test/java/core/framework/impl/inject/KeyTest.java
+++ core-ng/src/test/java/core/framework/impl/inject/KeyTest.java
@@ -2,22 +2,23 @@ package core.framework.impl.inject;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class KeyTest {
     @Test
     void asMapKey() {
         Key stringKey1 = new Key(String.class, null);
         Key stringKey2 = new Key(String.class, null);
-        assertEquals(stringKey1, stringKey2);
-        assertEquals(stringKey1.hashCode(), stringKey2.hashCode());
+        assertThat(stringKey1)
+                .isEqualTo(stringKey2)
+                .hasSameHashCodeAs(stringKey2);
 
         Key stringKey3 = new Key(String.class, "name");
         Key stringKey4 = new Key(String.class, "name");
-        assertEquals(stringKey3, stringKey4);
-        assertEquals(stringKey3.hashCode(), stringKey4.hashCode());
+        assertThat(stringKey3)
+                .isEqualTo(stringKey4)
+                .hasSameHashCodeAs(stringKey4);
 
-        assertNotEquals(new Key(String.class, null), new Key(int.class, null));
+        assertThat(new Key(String.class, null)).isNotEqualTo(new Key(int.class, null));
     }
 }
--- core-ng/src/test/java/core/framework/web/CookieSpecTest.java
+++ core-ng/src/test/java/core/framework/web/CookieSpecTest.java
@@ -11,11 +11,12 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class CookieSpecTest {
     @Test
-    void equals() {
+    void asMapKey() {
         CookieSpec spec1 = new CookieSpec("name").domain("example.com").path("/");
         CookieSpec spec2 = new CookieSpec("name").domain("example.com").path("/").maxAge(Duration.ofSeconds(30));
 
-        assertThat(spec1).isEqualTo(spec2);
-        assertThat(spec1).hasSameHashCodeAs(spec2);
+        assertThat(spec1)
+                .isEqualTo(spec2)
+                .hasSameHashCodeAs(spec2);
     }
 }
