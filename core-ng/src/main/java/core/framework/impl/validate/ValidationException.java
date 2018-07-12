a1048988adfc21eaa1fdcced42a3e2ef975f75de
neo <neowu.us@gmail.com>
2018-07-12 18:11:24 -0400

    update validation exception errorCode

--- core-ng/src/main/java/core/framework/impl/validate/ValidationException.java
+++ core-ng/src/main/java/core/framework/impl/validate/ValidationException.java
@@ -1,5 +1,6 @@
 package core.framework.impl.validate;
 
+import core.framework.log.ErrorCode;
 import core.framework.util.Strings;
 
 import java.util.Map;
@@ -7,12 +8,17 @@ import java.util.Map;
 /**
  * @author neo
  */
-public final class ValidationException extends RuntimeException {
+public final class ValidationException extends RuntimeException implements ErrorCode {
     private static final long serialVersionUID = -2784801858422440222L;
     public final Map<String, String> errors;
 
     public ValidationException(Map<String, String> errors) {
         super(Strings.format("validation failed, error={}", errors));
         this.errors = errors;
+    }
+
+    @Override
+    public String errorCode() {
+        return "VALIDATION_ERROR";
     }
 }
