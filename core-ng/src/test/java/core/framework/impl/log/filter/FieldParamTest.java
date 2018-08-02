abe2a897927b2146513b7027542cc222e3b3dc17
neo <neowu.us@gmail.com>
2018-08-02 17:01:49 +0800

    fix test

--- core-ng/src/test/java/core/framework/impl/log/filter/FieldParamTest.java
+++ core-ng/src/test/java/core/framework/impl/log/filter/FieldParamTest.java
@@ -1,6 +1,5 @@
 package core.framework.impl.log.filter;
 
-import core.framework.util.Sets;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -15,22 +14,21 @@ class FieldParamTest {
     @Test
     void filter() {
         var param = new FieldParam("SessionId", "value");
-        String message = param.filter(Set.of(new String[]{"SessionId"}));
+        String message = param.filter(Set.of("SessionId"));
         assertThat(message).isNotEqualTo("value");
     }
 
     @Test
     void formatArrayValue() {
         var param = new FieldParam("key", new Object[]{1, 2, 3});
-        String message = param.filter(Sets.newHashSet());
+        String message = param.filter(Set.of());
         assertThat(message).isEqualTo("[1, 2, 3]");
     }
 
     @Test
     void formatMapValue() {
-        var value = Map.of("k1", "v1", "k2", "v2");
-        var param = new FieldParam("key", value);
-        String message = param.filter(Sets.newHashSet());
-        assertThat(message).isEqualTo("{k1=v1, k2=v2}");
+        var param = new FieldParam("key", Map.of("k1", "v1"));
+        String message = param.filter(Set.of());
+        assertThat(message).contains("k1=v1").contains("{k1=v1}");
     }
 }
