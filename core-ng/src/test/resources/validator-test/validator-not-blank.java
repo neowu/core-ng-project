e0896f698b471eab620d0703f160d80bbea93aff
neo <neowu.us@gmail.com>
2018-09-30 22:05:55 -0400

    fix unit test

--- core-ng/src/test/resources/validator-test/validator-not-blank.java
+++ core-ng/src/test/resources/validator-test/validator-not-blank.java
@@ -1,18 +1,18 @@
-public class BeanValidatorNotEmptyTest$Bean$Validator implements core.framework.impl.validate.BeanValidator {
-    private void validateBean0(core.framework.impl.validate.BeanValidatorNotEmptyTest.Bean bean, core.framework.impl.validate.ValidationErrors errors, boolean partial) {
+public class BeanValidatorNotBlankTest$Bean$Validator implements core.framework.impl.validate.BeanValidator {
+    private void validateBean0(core.framework.impl.validate.BeanValidatorNotBlankTest.Bean bean, core.framework.impl.validate.ValidationErrors errors, boolean partial) {
         if (bean.stringField1 == null) {
             if (!partial) errors.add("stringField1", "field must not be null");
         } else {
-            if (bean.stringField1.isBlank()) errors.add("stringField1", "stringField1 must not be empty");
+            if (bean.stringField1.isBlank()) errors.add("stringField1", "stringField1 must not be blank");
         }
         if (bean.stringField2 == null) {
         } else {
-            if (bean.stringField2.isBlank()) errors.add("stringField2", "stringField2 must not be empty");
+            if (bean.stringField2.isBlank()) errors.add("stringField2", "stringField2 must not be blank");
         }
     }
 
     public void validate(Object instance, core.framework.impl.validate.ValidationErrors errors, boolean partial) {
-        validateBean0((core.framework.impl.validate.BeanValidatorNotEmptyTest.Bean) instance, errors, partial);
+        validateBean0((core.framework.impl.validate.BeanValidatorNotBlankTest.Bean) instance, errors, partial);
     }
 
 }
