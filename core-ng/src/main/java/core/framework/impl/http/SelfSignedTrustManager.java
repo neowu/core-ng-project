7f173b2bd8c8938e91e31653a3b7ee39b3175786
neo <neowu.us@gmail.com>
2018-10-01 17:25:22 -0400

    fix checkstyle

--- core-ng/src/main/java/core/framework/impl/http/SelfSignedTrustManager.java
+++ core-ng/src/main/java/core/framework/impl/http/SelfSignedTrustManager.java
@@ -10,15 +10,23 @@ import java.security.cert.X509Certificate;
  */
 public class SelfSignedTrustManager extends X509ExtendedTrustManager {
     @Override
+    public void checkClientTrusted(X509Certificate[] chain, String authType) {
+    }
+
+    @Override
     public void checkClientTrusted(X509Certificate[] chain, String authType, Socket socket) {
     }
 
     @Override
-    public void checkServerTrusted(X509Certificate[] chain, String authType, Socket socket) {
+    public void checkClientTrusted(X509Certificate[] chain, String authType, SSLEngine engine) {
     }
 
     @Override
-    public void checkClientTrusted(X509Certificate[] chain, String authType, SSLEngine engine) {
+    public void checkServerTrusted(X509Certificate[] chain, String authType) {
+    }
+
+    @Override
+    public void checkServerTrusted(X509Certificate[] chain, String authType, Socket socket) {
     }
 
     @Override
@@ -28,14 +36,6 @@ public class SelfSignedTrustManager extends X509ExtendedTrustManager {
     @Override
     public X509Certificate[] getAcceptedIssuers() {
         return new X509Certificate[0];
-    }
-
-    @Override
-    public void checkServerTrusted(final X509Certificate[] chain, final String authType) {
-    }
-
-    @Override
-    public void checkClientTrusted(final X509Certificate[] chain, final String authType) {
     }
 }
 
