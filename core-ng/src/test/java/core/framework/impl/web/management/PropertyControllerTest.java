5aff91f092d315188602269d52d3a5f5496d1661
neo <neowu.us@gmail.com>
2018-10-06 23:51:48 -0400

    update unit test

--- core-ng/src/test/java/core/framework/impl/web/management/PropertyControllerTest.java
+++ core-ng/src/test/java/core/framework/impl/web/management/PropertyControllerTest.java
@@ -1,10 +1,15 @@
 package core.framework.impl.web.management;
 
+import core.framework.http.ContentType;
 import core.framework.impl.module.PropertyManager;
+import core.framework.web.Request;
+import core.framework.web.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 /**
  * @author neo
@@ -17,6 +22,15 @@ class PropertyControllerTest {
     void createPropertyController() {
         propertyManager = new PropertyManager();
         controller = new PropertyController(propertyManager);
+    }
+
+    @Test
+    void execute() {
+        Request request = Mockito.mock(Request.class);
+        when(request.clientIP()).thenReturn("127.0.0.1");
+
+        Response response = controller.execute(request);
+        assertThat(response.contentType()).get().isEqualTo(ContentType.TEXT_PLAIN);
     }
 
     @Test
