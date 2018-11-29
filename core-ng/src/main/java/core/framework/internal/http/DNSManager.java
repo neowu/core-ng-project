7e6bf156b58ecb73735353317a5def0bfc866923
neo <neowu.us@gmail.com>
2018-11-29 15:00:42 +0800

    fix code check

--- core-ng/src/main/java/core/framework/internal/http/DNSManager.java
+++ core-ng/src/main/java/core/framework/internal/http/DNSManager.java
@@ -1,6 +1,5 @@
 package core.framework.internal.http;
 
-import core.framework.http.HTTPClientException;
 import okhttp3.Dns;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,31 +34,40 @@ public class DNSManager implements Dns {
 
     @Override
     public List<InetAddress> lookup(String hostname) throws UnknownHostException {
+        Lookup lookup = createLookup(hostname);
+        Record[] records = lookup.run();
+        if (records == null || lookup.getResult() != Lookup.SUCCESSFUL) {
+            logger.warn("failed to resolve host, host={}, fallback to system DNS", hostname);
+            return List.of(InetAddress.getAllByName(hostname));
+        }
+        List<InetAddress> addresses = new ArrayList<>(records.length);
+        for (Record record : records) {
+            int type = record.getType();
+            switch (type) {
+                case Type.A:
+                    addresses.add(((ARecord) record).getAddress());
+                    break;
+                case Type.AAAA:
+                    addresses.add(((AAAARecord) record).getAddress());
+                    break;
+                default:
+                    throw new Error("not supported record type, type=" + type);
+            }
+        }
+        return addresses;
+    }
+
+    @SuppressWarnings("AvoidHidingCauseException")
+    Lookup createLookup(String hostname) throws UnknownHostException {
+        if (hostname == null) throw new UnknownHostException("hostname must not be null");
         try {
             var lookup = new Lookup(hostname);
             lookup.setResolver(resolver);
-            Record[] records = lookup.run();
-            if (records == null || lookup.getResult() != Lookup.SUCCESSFUL) {
-                logger.warn("failed to resolve host, host={}, fallback to system DNS", hostname);
-                return List.of(InetAddress.getAllByName(hostname));
-            }
-            List<InetAddress> addresses = new ArrayList<>(records.length);
-            for (Record record : records) {
-                int type = record.getType();
-                switch (type) {
-                    case Type.A:
-                        addresses.add(((ARecord) record).getAddress());
-                        break;
-                    case Type.AAAA:
-                        addresses.add(((AAAARecord) record).getAddress());
-                        break;
-                    default:
-                        throw new Error("not supported record type, type=" + type);
-                }
-            }
-            return addresses;
+            return lookup;
         } catch (TextParseException e) {
-            throw new HTTPClientException("invalid host, host=" + hostname, "INVALID_URL", e);
+            var exception = new UnknownHostException("failed to parse hostname, host=" + hostname);
+            exception.initCause(e);
+            throw exception;
         }
     }
 }
