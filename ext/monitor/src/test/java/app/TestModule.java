4ead9bec25d0eec32bfba6866f9ab07d0bcc2f59
neo <neowu.us@gmail.com>
2020-02-04 14:24:08 -0500

    fix unit test

--- ext/monitor/src/test/java/app/TestModule.java
+++ ext/monitor/src/test/java/app/TestModule.java
@@ -1,15 +1,11 @@
 package app;
 
-import core.framework.http.HTTPClient;
 import core.framework.test.module.AbstractTestModule;
 import core.framework.util.ClasspathResources;
-
-import static org.mockito.Mockito.mock;
 
 public class TestModule extends AbstractTestModule {
     @Override
     protected void initialize() {
-        overrideBinding(HTTPClient.class, mock(HTTPClient.class));
         System.setProperty("app.monitor.config", ClasspathResources.text("monitor.json"));
 
         load(new MonitorApp());
