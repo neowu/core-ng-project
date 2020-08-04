36143095924c3f773e1a651070fb5e6e81a90f1e
neo <neowu.us@gmail.com>
2020-08-04 11:07:30 -0400

    update unit test

--- core-ng/src/test/java/core/framework/internal/kafka/ConsumerMetricsTest.java
+++ core-ng/src/test/java/core/framework/internal/kafka/ConsumerMetricsTest.java
@@ -1,5 +1,6 @@
 package core.framework.internal.kafka;
 
+import core.framework.internal.stat.Stats;
 import org.apache.kafka.common.Metric;
 import org.junit.jupiter.api.Test;
 
@@ -16,7 +17,7 @@ import static org.mockito.Mockito.when;
 class ConsumerMetricsTest {
     @Test
     void statName() {
-        ConsumerMetrics metrics = new ConsumerMetrics("log");
+        var metrics = new ConsumerMetrics("log");
         assertEquals("kafka_consumer_log_lag_max", metrics.statName("lag_max"));
 
         metrics = new ConsumerMetrics(null);
@@ -32,5 +33,17 @@ class ConsumerMetricsTest {
 
         var metrics = new ConsumerMetrics(null);
         assertThat(metrics.sum(List.of(metric1, metric1, metric2))).isEqualTo(2);
+    }
+
+    @Test
+    void collect() {
+        var metrics = new ConsumerMetrics(null);
+        var stats = new Stats();
+        metrics.collect(stats);
+
+        assertThat(stats.stats).containsKeys("kafka_consumer_records_max_lag",
+                "kafka_consumer_records_consumed_rate",
+                "kafka_consumer_bytes_consumed_rate",
+                "kafka_consumer_fetch_rate");
     }
 }
