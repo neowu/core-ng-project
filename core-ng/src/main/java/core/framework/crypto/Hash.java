5b5ab6f994b3aa07e8dbed52bb8d082f6b064b54
neo <neowu.us@gmail.com>
2020-11-24 12:03:14 -0500

    test github actions as travis CI is restricting free OSS projects

--- core-ng/src/main/java/core/framework/crypto/Hash.java
+++ core-ng/src/main/java/core/framework/crypto/Hash.java
@@ -11,7 +11,7 @@ import java.security.NoSuchAlgorithmException;
 public final class Hash {
     private static final char[] HEX_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
-    // md5/sha1 is not considered as secure algorithm, only used on legacy or security insensitive cases, like checksum or masking
+    // md5/sha1 are not considered as secure algorithm, only used on legacy or security insensitive cases, like checksum or masking
     public static String md5Hex(byte[] value) {
         return hash(value, "MD5");
     }
