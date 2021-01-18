5b5d2b10821e8d78d14c45056d252f9263e66104
neo <neowu.us@gmail.com>
2021-01-18 12:13:52 -0500

    refactor unit test

--- core-ng/src/test/java/core/framework/internal/web/service/WebServiceClientLinkContextTest.java
+++ core-ng/src/test/java/core/framework/internal/web/service/WebServiceClientLinkContextTest.java
@@ -20,33 +20,55 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class WebServiceClientLinkContextTest {
     private WebServiceClient webServiceClient;
+    private LogManager logManager;
 
     @BeforeEach
     void createWebServiceClient() {
+        logManager = new LogManager();
         var httpClient = new HTTPClientImpl(null, null, Duration.ofSeconds(10), Duration.ofSeconds(20));
         webServiceClient = new WebServiceClient("http://localhost", httpClient, new RequestBeanWriter(), new ResponseBeanReader());
     }
 
     @Test
     void linkContext() {
-        var logManager = new LogManager();
         ActionLog actionLog = logManager.begin("begin", null);
-        actionLog.maxProcessTimeInNano = Duration.ofSeconds(1).toNanos();
+        actionLog.maxProcessTimeInNano = Duration.ofSeconds(60).toNanos();
 
         var request = new HTTPRequest(HTTPMethod.GET, "http://localhost");
         webServiceClient.linkContext(request);
         assertThat(request.headers).containsKeys(HTTPHandler.HEADER_CLIENT.toString(),
                 HTTPHandler.HEADER_CORRELATION_ID.toString(),
-                HTTPHandler.HEADER_REF_ID.toString(),
-                HTTPHandler.HEADER_TIMEOUT.toString());
-        assertThat(Long.parseLong(request.headers.get(HTTPHandler.HEADER_TIMEOUT.toString()))).isGreaterThan(0).isLessThanOrEqualTo(Duration.ofSeconds(1).toNanos());
+                HTTPHandler.HEADER_REF_ID.toString());
 
-        actionLog.maxProcessTimeInNano = Duration.ofSeconds(60).toNanos();
-        request = new HTTPRequest(HTTPMethod.GET, "http://localhost");
-        webServiceClient.linkContext(request);
         assertThat(request.headers)
                 .as("should use http client timeout if it is shorter than max process time")
                 .containsEntry(HTTPHandler.HEADER_TIMEOUT.toString(), "20000000000");
+
+        logManager.end("end");
+    }
+
+    @Test
+    void linkContextWithoutMaxProcessTime() {
+        logManager.begin("begin", null);
+
+        var request = new HTTPRequest(HTTPMethod.GET, "http://localhost");
+        webServiceClient.linkContext(request);
+        assertThat(request.headers).containsEntry(HTTPHandler.HEADER_TIMEOUT.toString(), "20000000000");
+
+        logManager.end("end");
+    }
+
+    @Test
+    void linkContextWithShortProcessTime() {
+        ActionLog actionLog = logManager.begin("begin", null);
+        actionLog.maxProcessTimeInNano = Duration.ofSeconds(1).toNanos();
+
+        var request = new HTTPRequest(HTTPMethod.GET, "http://localhost");
+        request = new HTTPRequest(HTTPMethod.GET, "http://localhost");
+        webServiceClient.linkContext(request);
+        assertThat(Long.parseLong(request.headers.get(HTTPHandler.HEADER_TIMEOUT.toString())))
+                .isGreaterThan(0)
+                .isLessThanOrEqualTo(Duration.ofSeconds(1).toNanos());
 
         logManager.end("end");
     }
