22b4d58d40cff63c42372bbfb28bdae37801dd1e
neo <neowu.us@gmail.com>
2021-01-25 13:15:41 -0500

    * http: collect http server active requests to reflect how many active requests are in process (to analyze performance with cpu usage, worker thread pool size and etc)

--- core-ng/src/main/java/core/framework/internal/web/ShutdownHandler.java
+++ core-ng/src/main/java/core/framework/internal/web/ShutdownHandler.java
@@ -6,20 +6,22 @@ import io.undertow.util.StatusCodes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author neo
  */
 public class ShutdownHandler implements ExchangeCompletionListener {
-    final AtomicLong activeRequests = new AtomicLong(0);
+    final AtomicInteger activeRequests = new AtomicInteger(0);
 
     private final Logger logger = LoggerFactory.getLogger(ShutdownHandler.class);
     private final Object lock = new Object();
+    private final AtomicInteger maxActiveRequests = new AtomicInteger(0);
     private volatile boolean shutdown;
 
     boolean handle(HttpServerExchange exchange) {
-        activeRequests.getAndIncrement();
+        int current = activeRequests.incrementAndGet();
+        maxActiveRequests.getAndAccumulate(current, Math::max);     // only increase active request triggers max active request process, doesn't need to handle when active requests decrease
         exchange.addExchangeCompleteListener(this);
 
         if (shutdown) {
@@ -33,6 +35,11 @@ public class ShutdownHandler implements ExchangeCompletionListener {
         }
 
         return false;
+    }
+
+    // return max/peak active requests since last call, and reset max with current value
+    int maxActiveRequests() {
+        return maxActiveRequests.getAndSet(activeRequests.get());
     }
 
     void shutdown() {
@@ -56,7 +63,7 @@ public class ShutdownHandler implements ExchangeCompletionListener {
     @Override
     public void exchangeEvent(HttpServerExchange exchange, NextListener next) {
         try {
-            long count = activeRequests.decrementAndGet();
+            int count = activeRequests.decrementAndGet();
             if (count <= 0 && shutdown) {
                 synchronized (lock) {
                     lock.notifyAll();
