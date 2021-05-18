21602ebb4b726381cce5773c8167a019af871feb
neo <neowu.us@gmail.com>
2021-05-18 14:33:13 -0400

    update es config, add unit test

--- ext/monitor/src/main/java/app/monitor/job/GCStat.java
+++ ext/monitor/src/main/java/app/monitor/job/GCStat.java
@@ -16,13 +16,17 @@ public class GCStat {
         long previous = previousCount;
         previousCount = currentCollectionCount;
         if (previous == -1) return 0;   // ignore first data point, only count changes, as monitor just started
-        return currentCollectionCount - previous;
+        long count = currentCollectionCount - previous;
+        if (count < 0) return 0; // e.g. target jvm restarted, and monitor is still using old counter
+        return count;
     }
 
     long elapsed(long currentCollectionTime) {
         long previous = previousElapsed;
         previousElapsed = currentCollectionTime;
         if (previous == -1) return 0;   // ignore first data point, only count changes, as monitor just started
-        return (currentCollectionTime - previous) * 1_000_000; // convert to nano
+        long time = (currentCollectionTime - previous) * 1_000_000;
+        if (time < 0) return 0; // e.g. target jvm restarted, and monitor is still using old counter
+        return time; // convert to nano
     }
 }
