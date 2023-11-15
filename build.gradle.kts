d85b8ffc2145e402d44e7ba3447d5afec08e9f20
neo <1100909+neowu@users.noreply.github.com>
2023-11-15 10:47:44 +0800

    update jackson version to align with google cloud sdk
    Signed-off-by: neo <1100909+neowu@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ subprojects {
 
 val elasticVersion = "8.10.3"
 val kafkaVersion = "3.6.0"
-val jacksonVersion = "2.15.2"
+val jacksonVersion = "2.15.3"
 val junitVersion = "5.10.0"
 val mockitoVersion = "5.6.0"
 val assertjVersion = "3.24.2"
