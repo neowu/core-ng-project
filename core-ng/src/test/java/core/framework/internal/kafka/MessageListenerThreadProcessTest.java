e6af1c162fc2e6fcc727576f0676ed8ee2db561a
neo <1100909+neowu@users.noreply.github.com>
2024-02-05 11:32:36 +0800

    update gradle and enable pmd partially (pmd only supports java 21 fully in 7.0)
    Signed-off-by: neo <1100909+neowu@users.noreply.github.com>

--- core-ng/src/test/java/core/framework/internal/kafka/MessageListenerThreadProcessTest.java
+++ core-ng/src/test/java/core/framework/internal/kafka/MessageListenerThreadProcessTest.java
@@ -23,7 +23,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class MessageListenerThreadProcessTest {
+class MessageListenerThreadProcessTest {
     @Mock
     MessageHandler<TestMessage> messageHandler;
     @Mock
