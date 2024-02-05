a08fba9405d4658f18d6ae3d7ad33f6a706e96fc
neo <1100909+neowu@users.noreply.github.com>
2024-02-05 18:15:34 +0800

    refactor http server
    Signed-off-by: neo <1100909+neowu@users.noreply.github.com>

--- core-ng/src/main/java/core/framework/internal/web/HTTPServer.java
+++ core-ng/src/main/java/core/framework/internal/web/HTTPServer.java
@@ -70,8 +70,7 @@ public class HTTPServer {
                 .setServerOption(UndertowOptions.MAX_ENTITY_SIZE, config.maxEntitySize)
                 .setServerOption(UndertowOptions.RECORD_REQUEST_START_TIME, Boolean.TRUE);
 
-            Xnio xnio = Xnio.getInstance(Thread.currentThread().getContextClassLoader());
-            builder.setWorker(xnio.createWorkerBuilder()
+            builder.setWorker(Xnio.getInstance().createWorkerBuilder()
                 .setWorkerIoThreads(Math.max(Runtime.getRuntime().availableProcessors(), 2))
                 .setExternalExecutorService(handler.worker)
                 .build());
