d539be815aaaa1c4edada25ffbfe36e43186815e
neo <1100909+neowu@users.noreply.github.com>
2025-08-08 18:06:31 +0800

    update frontend gradle script to support config cache
    Signed-off-by: neo <1100909+neowu@users.noreply.github.com>

--- buildSrc/src/main/kotlin/frontend.gradle.kts
+++ buildSrc/src/main/kotlin/frontend.gradle.kts
@@ -4,38 +4,42 @@ tasks.named("mkdir") {
     }
 }
 
-interface ExecSupport {
+interface Context {
     @get:Inject
     val operation: ExecOperations
+
+    @get:Inject
+    val fs: FileSystemOperations
 }
 
 afterEvaluate {
     if (!project.extensions.extraProperties.has("frontendDir")) throw Error("project does not have frontendDir property, assign by project.ext[\"frontendDir\"]")
+
     val frontendDir = file(project.extensions.extraProperties.get("frontendDir") as String)
-    val support = project.objects.newInstance<ExecSupport>()
+    if (!frontendDir.exists()) throw Error("$frontendDir does not exist")
+
+    val context = project.objects.newInstance<Context>()
+    val env = project.properties["env"] as String? // use gradlew -Penv=${env} to pass
 
     tasks.register("buildFrontend") {
         group = "build"
         doLast {
-            if (!frontendDir.exists()) throw Error("$frontendDir does not exist")
-
-            support.operation.exec {
+            context.operation.exec {
                 workingDir(frontendDir)
                 commandLine(Frontend.commandLine(listOf("pnpm", "install")))
             }
 
             val command = mutableListOf("pnpm", "run", "build")
+            if (env != null) command.addAll(listOf("--env", env))
 
-            val env = project.properties["env"] // use gradlew -Penv=${env} to pass
-            if (env != null) command.addAll(listOf("--env", env as String))
-
-            support.operation.exec {
+            context.operation.exec {
                 workingDir(frontendDir)
                 commandLine(Frontend.commandLine(command))
             }
-
-            delete("src/main/dist/web")
-            copy {
+            context.fs.delete {
+                delete("src/main/dist/web")
+            }
+            context.fs.copy {
                 from("${frontendDir}/build/dist")
                 if (env != null) exclude("static/*")    // exclude everything in static folder on server env, but keep static folder if exists
                 into("src/main/dist/web")
