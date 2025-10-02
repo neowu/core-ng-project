fa6464a2be7b24ad643d2aaf38045dc1ce9e79ee
neo <1100909+neowu@users.noreply.github.com>
2025-10-02 11:48:26 +0800

    update gradle kotlin target version
    Signed-off-by: neo <1100909+neowu@users.noreply.github.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,9 +13,16 @@ java {
     }
 }
 
+kotlin {
+    compilerOptions {
+        // kotlin not support jvm 25 yet
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_24)
+    }
+}
+
 dependencies {
-    implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:6.1.13")
-    implementation("org.flywaydb:flyway-gradle-plugin:11.10.5")
-    runtimeOnly("org.flywaydb:flyway-mysql:11.10.5")
-    runtimeOnly("org.flywaydb:flyway-database-postgresql:11.10.5")
+    implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:6.2.5")
+    implementation("org.flywaydb:flyway-gradle-plugin:11.13.2")
+    runtimeOnly("org.flywaydb:flyway-mysql:11.13.2")
+    runtimeOnly("org.flywaydb:flyway-database-postgresql:11.13.2")
 }
