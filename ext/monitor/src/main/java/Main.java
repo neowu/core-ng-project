10c0563bcbfa982b1355f9d97540ba4432383e48
neo <1100909+neowu@users.noreply.github.com>
2025-10-08 14:30:08 +0800

    simplify main class
    Signed-off-by: neo <1100909+neowu@users.noreply.github.com>

--- ext/monitor/src/main/java/Main.java
+++ ext/monitor/src/main/java/Main.java
@@ -3,8 +3,6 @@ import app.MonitorApp;
 /**
  * @author ericchung
  */
-public class Main {
-    static void main() {
-        new MonitorApp().start();
-    }
+void main() {
+    new MonitorApp().start();
 }
