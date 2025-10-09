5c3e84a05578252687b28a8abea41db411385262
neo <1100909+neowu@users.noreply.github.com>
2025-10-09 10:05:33 +0800

    tweak junit 6 integration test context initialization
    Signed-off-by: neo <1100909+neowu@users.noreply.github.com>

--- core-ng-test/src/main/java/core/framework/test/IntegrationExtension.java
+++ core-ng-test/src/main/java/core/framework/test/IntegrationExtension.java
@@ -4,27 +4,41 @@ import core.framework.test.module.AbstractTestModule;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestInstancePostProcessor;
 
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author neo
  */
 public final class IntegrationExtension implements TestInstancePostProcessor {
-    private static final AtomicBoolean INITIALIZED = new AtomicBoolean(false);
+    private static final ReentrantLock LOCK = new ReentrantLock();
 
     @Override
     public void postProcessTestInstance(Object testInstance, ExtensionContext context) {
         ExtensionContext.Store store = context.getRoot().getStore(ExtensionContext.Namespace.GLOBAL);
         Class<?> testClass = context.getRequiredTestClass();
-        AbstractTestModule module = store.computeIfAbsent(AbstractTestModule.class, _ -> createTestModule(testClass), AbstractTestModule.class);
+        AbstractTestModule module = getTestModule(store, testClass);
         module.inject(testInstance);
     }
 
-    private AbstractTestModule createTestModule(Class<?> testClass) {
-        boolean initialized = INITIALIZED.get();    // junit 6 store doesn't support nested update, use global atomic to check loosely in test env
-        if (initialized) throw new Error("test context failed to initialize, please check error message from previous integration test");
-        INITIALIZED.set(true);
+    private AbstractTestModule getTestModule(ExtensionContext.Store store, Class<?> testClass) {
+        try {
+            LOCK.lock();
+            AbstractTestModule module = store.get(AbstractTestModule.class, AbstractTestModule.class);
+            if (module == null) {
+                if (Boolean.TRUE.equals(store.get("initialized", Boolean.class))) {
+                    throw new Error("test context failed to initialize, please check error message from previous integration test");
+                }
+                store.put("initialized", Boolean.TRUE);
+                module = createTestModule(testClass);
+                store.put(AbstractTestModule.class, module);
+            }
+            return module;
+        } finally {
+            LOCK.unlock();
+        }
+    }
 
+    private AbstractTestModule createTestModule(Class<?> testClass) {
         Context context = findContext(testClass);
         try {
             AbstractTestModule module = context.module().getConstructor().newInstance();
