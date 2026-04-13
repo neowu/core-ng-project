5791bdb2d62a322c89f8e426bfb13ae20cad60a2
neo <1100909+neowu@users.noreply.github.com>
2026-04-13 18:08:47 -0400

    * es: added es_active_requests stats to monitor es concurrency   > search.maxConnections limits the number of requests processed at same time
    Signed-off-by: neo <1100909+neowu@users.noreply.github.com>

--- core-ng-search/src/test/java/core/framework/search/impl/ElasticSearchMetricsTest.java
+++ core-ng-search/src/test/java/core/framework/search/impl/ElasticSearchMetricsTest.java
@@ -1,38 +1,29 @@
 package core.framework.search.impl;
 
 import core.framework.internal.stat.Stats;
-import org.apache.hc.client5.http.HttpRoute;
-import org.apache.hc.core5.pool.ConnPoolStats;
-import org.apache.hc.core5.pool.PoolStats;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
 class ElasticSearchMetricsTest {
-    @Mock
-    ConnPoolStats<HttpRoute> poolStats;
+    private LogInstrumentation instrumentation;
     private ElasticSearchMetrics metrics;
 
     @BeforeEach
-    void createElasticSearchConnectionPoolMetrics() {
-        metrics = new ElasticSearchMetrics(null);
-        metrics.poolStats = poolStats;
+    void createElasticSearchMetrics() {
+        instrumentation = new LogInstrumentation();
+
+        metrics = new ElasticSearchMetrics(null, instrumentation);
     }
 
     @Test
     void collect() {
-        when(poolStats.getTotalStats()).thenReturn(new PoolStats(5, 0, 0, 100));
+        instrumentation.activeRequests.increase();
 
         var stats = new Stats();
         metrics.collect(stats);
 
-        assertThat(stats.stats).containsEntry("pool_es_active_count", 5.0)
-            .containsEntry("pool_es_total_count", 100.0);
+        assertThat(stats.stats).containsEntry("es_active_requests", 1.0);
     }
 }
